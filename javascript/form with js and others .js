e773df18705ee97dc170c0af78cd3dc39a231895
Atir Sarwar <atir.cit@gmail.com>
2025-10-19 12:01:07 +0500

    update 'form with js and others.css', index.html and 'form with js and others .js'

--- javascript/form with js and others .js
+++ javascript/form with js and others .js
@@ -1,103 +1,91 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-  if (localStorage.getItem("email")) {
+  if(localStorage.getItem("email")){
     $("#signupForm").hide()
     $("#loginForm").show()
-  } 
-  
-  else {
+  }
+  else{
     $("#signupForm").show()
     $("#loginForm").hide()
   }
 
-  $("#email").on("input", function () {
-    let val = $(this).val()
-    let icon = $("#emailIcon")
-    if (val.includes("@") && val.includes(".")) {
-      icon.removeClass("fa-times text-danger").addClass("fa-check text-success")
-    } 
-    
-    else {
-      icon.removeClass("fa-check text-success").addClass("fa-times text-danger")
+  $("#email").keyup(function(){
+    let emailValue = $("#email").val()
+    if(emailValue.includes("@") && emailValue.includes(".")){
+      $("#emailIcon").removeClass("fa-times text-danger").addClass("fa-check text-success")
+    }
+    else{
+      $("#emailIcon").removeClass("fa-check text-success").addClass("fa-times text-danger")
     }
   })
 
-  $("#password").on("input", function () {
-    let val = $(this).val()
-    let icon = $("#passwordIcon")
-    if (val.length >= 8) {
-      icon.removeClass("fa-times text-danger").addClass("fa-check text-success")
-    } 
-    
-    else {
-      icon.removeClass("fa-check text-success").addClass("fa-times text-danger")
+  $("#password").keyup(function(){
+    let passwordValue = $("#password").val()
+    if(passwordValue.length >= 8){
+      $("#passwordIcon").removeClass("fa-times text-danger").addClass("fa-check text-success")
+    }
+    else{
+      $("#passwordIcon").removeClass("fa-check text-success").addClass("fa-times text-danger")
     }
   })
 
+  $("#loginEmail").keyup(function(){
+    let loginEmailValue = $("#loginEmail").val()
+    if(loginEmailValue.includes("@") && loginEmailValue.includes(".")){
+      $("#loginEmailIcon").removeClass("fa-times text-danger").addClass("fa-check text-success")
+    }
+    else{
+      $("#loginEmailIcon").removeClass("fa-check text-success").addClass("fa-times text-danger")
+    }
+  })
 
-  $("#signupBtn").click(function () {
-    let uname = $("#uname").val()
+  $("#loginPassword").keyup(function(){
+    let loginPasswordValue = $("#loginPassword").val()
+    if(loginPasswordValue.length >= 8){
+      $("#loginPasswordIcon").removeClass("fa-times text-danger").addClass("fa-check text-success")
+    }
+    else{
+      $("#loginPasswordIcon").removeClass("fa-check text-success").addClass("fa-times text-danger")
+    }
+  })
+
+  $("#signupBtn").click(function(){
+    let userName = $("#uname").val()
     let email = $("#email").val()
     let password = $("#password").val()
 
-    if (uname === "" || email === "" || password === "") {
+    if(userName == "" || email == "" || password == ""){
       alert("Please fill all fields!")
       return
     }
 
-    localStorage.setItem("uname", uname)
+    localStorage.setItem("uname", userName)
     localStorage.setItem("email", email)
     localStorage.setItem("password", password)
 
     alert("Signup successful! Now login.")
     $("#signupForm").hide()
     $("#loginForm").show()
-
-    $("#uname, #email, #password").val("")
+    $("#uname,#email,#password").val("")
   })
 
-  $("#loginEmail").on("input", function () {
-    let val = $(this).val()
-    let icon = $("#loginEmailIcon")
-    if (val.includes("@") && val.includes(".")) {
-      icon.removeClass("fa-times text-danger").addClass("fa-check text-success")
-    } 
-    
-    else {
-      icon.removeClass("fa-check text-success").addClass("fa-times text-danger")
-    }
-  })
-
-  $("#loginPassword").on("input", function () {
-    let val = $(this).val()
-    let icon = $("#loginPasswordIcon")
-    if (val.length >= 8) {
-      icon.removeClass("fa-times text-danger").addClass("fa-check text-success")
-    } 
-    else {
-      icon.removeClass("fa-check text-success").addClass("fa-times text-danger")
-    }
-  })
-
-  $("#loginBtn").click(function () {
-    let email = $("#loginEmail").val()
-    let pass = $("#loginPassword").val()
-
+  $("#loginBtn").click(function(){
+    let loginEmail = $("#loginEmail").val()
+    let loginPassword = $("#loginPassword").val()
     let storedEmail = localStorage.getItem("email")
-    let storedPass = localStorage.getItem("password")
+    let storedPassword = localStorage.getItem("password")
     let storedName = localStorage.getItem("uname")
 
-    if (email === storedEmail && pass === storedPass) {
+    if(loginEmail == storedEmail && loginPassword == storedPassword){
       alert("Welcome back, " + storedName + "!")
-      $("#loginEmail, #loginPassword").val("")
-    } 
-    
-    else {
+      $("#loginEmail,#loginPassword").val("")
+    }
+    else{
       alert("Invalid email or password!")
     }
   })
 
-  $("#clearBtn").click(function () {
+  $("#clearBtn").click(function(){
     localStorage.clear()
     alert("Storage cleared. Starting fresh!")
     $("#loginForm").hide()
